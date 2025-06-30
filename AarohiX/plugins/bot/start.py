bb03d10be9d6ba88118baedb9011d9710eac7005
tmmteam <teamtmm2@gmail.com>
2025-06-30 12:16:34 +0530

    Update start.py

--- AarohiX/plugins/bot/start.py
+++ AarohiX/plugins/bot/start.py
@@ -1,173 +1,391 @@
 import time
+
 import random
+
 from pyrogram import filters
+
 from pyrogram.enums import ChatType
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
 from youtubesearchpython.__future__ import VideosSearch
 
+
+
 import config
+
 from AarohiX import app
+
 from AarohiX.misc import _boot_
+
 from AarohiX.plugins.sudo.sudoers import sudoers_list
+
 from AarohiX.utils import bot_sys_stats
+
 from AarohiX.utils.database import (
+
     add_served_chat,
+
     add_served_user,
+
     blacklisted_chats,
+
     get_lang,
+
     is_banned_user,
+
     is_on_off,
+
 )
+
 from AarohiX.utils.decorators.language import LanguageStart
+
 from AarohiX.utils.formatters import get_readable_time
+
 from AarohiX.utils.inline import first_page, private_panel, start_panel
+
 from config import BANNED_USERS
+
 from strings import get_string
 
 
+
+
+
 #<<<<<<<<<<<<<pics>>>>>>>>>>>#
 
+
+
 """
+
 ADISA_PICS = [
-"https://graph.org/file/e0c7e04cc0acff425fe5d.jpg",
-"https://graph.org/file/0f256eac6f4a8a053193a.jpg",
-"https://graph.org/file/e3babc6e511746be05204.jpg",
-"https://graph.org/file/323acfeef219c919091ec.jpg",
-"https://graph.org/file/1f34f63999a5599051b94.jpg",
-"https://graph.org/file/528b50c44cbfedda9c77e.jpg",
-"https://graph.org/file/365b3ab63ccd789f99bb4.jpg"
+
+"https://graph.org/file/9bba2b7ee9ba3806de65d.jpg",
+
+    "https://graph.org/file/ef82f289043a4fa74f8ff.jpg",
+
+    "https://graph.org/file/9c27c68958e06ae074c38.jpg",
+
+    "https://graph.org/file/0ff325b1d2efe80299aa3.jpg",
+
+    "https://graph.org/file/41167b953cf3579853d47.jpg",
+
+    "https://graph.org/file/bd93ab42e69305f274028.jpg",
+
+    "https://graph.org/file/97575db5586c05d6b2898.jpg",
+
+    "https://graph.org/file/07c393fdf931a407c9bc0.jpg",
+
+    "https://graph.org/file/f332767490ad3a5ca20e8.jpg",
+
+    "https://graph.org/file/f3449e9069667f647d14e.jpg",
+
+    "https://graph.org/file/9f51cdc739f907cbd2c7e.jpg",
+
+    "https://telegra.ph/file/d7a6a923c38e051ce35f3.jpg",
+
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg",
+
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",    
+
 ]
 
 
+
+
+
 #<<<<<<<<<<<<<pics>>>>>>>>>>>#
+
 """
 
 
+
+
+
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+
 @LanguageStart
+
 async def start_pm(client, message: Message, _):
+
     await add_served_user(message.from_user.id)
+
     if len(message.text.split()) > 1:
+
         name = message.text.split(None, 1)[1]
+
         if name[0:4] == "help":
+
             keyboard = first_page(_)
-            await message.reply_sticker("CAACAgUAAx0CcSyO9gACl7dkQoL0p1vXqcYtCuCHmSyh4Hi1UgACEwgAAonAEVYwaJ0Jkk8L0B4E")
+
+            await message.reply_sticker("CAACAgUAAx0CfLoeOQACaZVmIIOAjVdqAXMIZRhQ3uselOvotQAC4RAAAkzCcVSCoM-aLK5MsR4E")
+
             return await message.reply_photo(
+
                 photo=config.START_IMG_URL,
+
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
+
                 reply_markup=keyboard,
+
             )
+
         if name[0:3] == "sud":
+
             await sudoers_list(client=client, message=message, _=_)
+
             if await is_on_off(2):
+
                 return await app.send_message(
+
                     chat_id=config.LOGGER_ID,
+
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+
                 )
+
             return
+
         if name[0:3] == "inf":
+
             m = await message.reply_text("**» sᴇᴀʀᴄʜɪɴɢ ʙᴀʙʏ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ")
+
             query = (str(name)).replace("info_", "", 1)
+
             query = f"https://www.youtube.com/watch?v={query}"
+
             results = VideosSearch(query, limit=1)
+
             for result in (await results.next())["result"]:
+
                 title = result["title"]
+
                 duration = result["duration"]
+
                 views = result["viewCount"]["short"]
+
                 thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+
                 channellink = result["channel"]["link"]
+
                 channel = result["channel"]["name"]
+
                 link = result["link"]
+
                 published = result["publishedTime"]
+
             searched_text = _["start_6"].format(
+
                 title, duration, views, published, channellink, channel, app.mention
+
             )
+
             key = InlineKeyboardMarkup(
+
                 [
+
                     [
+
                         InlineKeyboardButton(text=_["S_B_8"], url=link),
+
                         InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+
                     ],
+
                 ]
+
             )
+
             await m.delete()
+
             await app.send_photo(
+
                 chat_id=message.chat.id,
+
                 photo=thumbnail,
+
                 caption=searched_text,
+
                 reply_markup=key,
+
             )
+
             if await is_on_off(2):
+
                 return await app.send_message(
+
                     chat_id=config.LOGGER_ID,
+
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+
                 )
+
     else:
+
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAx0CcSyO9gACl7dkQoL0p1vXqcYtCuCHmSyh4Hi1UgACEwgAAonAEVYwaJ0Jkk8L0B4E")
+
+        await message.reply_sticker("CAACAgUAAx0CfLoeOQACaZVmIIOAjVdqAXMIZRhQ3uselOvotQAC4RAAAkzCcVSCoM-aLK5MsR4E")
+
         await message.reply_photo(
+
             photo=config.START_IMG_URL,
+
             caption=_["start_2"].format(message.from_user.mention, app.mention),
+
             reply_markup=InlineKeyboardMarkup(out),
+
         )
+
         if await is_on_off(2):
+
             return await app.send_message(
+
                 chat_id=config.LOGGER_ID,
+
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+
             )
+
+
+
 
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
+
 @LanguageStart
+
 async def start_gp(client, message: Message, _):
+
     out = start_panel(_)
+
     uptime = int(time.time() - _boot_)
+
     await message.reply_photo(
+
         photo=config.START_IMG_URL,
+
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
+
         reply_markup=InlineKeyboardMarkup(out),
+
     )
+
     return await add_served_chat(message.chat.id)
 
 
+
+
+
 @app.on_message(filters.new_chat_members, group=-1)
+
 async def welcome(client, message: Message):
+
     for member in message.new_chat_members:
+
         try:
+
             language = await get_lang(message.chat.id)
+
             _ = get_string(language)
+
             if await is_banned_user(member.id):
+
                 try:
+
                     await message.chat.ban_member(member.id)
+
                 except:
+
                     pass
+
             if member.id == app.id:
+
                 if message.chat.type != ChatType.SUPERGROUP:
+
                     await message.reply_text(_["start_4"])
-                    return await app.leave_chat(message.chat.id)
-                if message.chat.id in await blacklisted_chats():
-                    await message.reply_text(
-                        _["start_5"].format(
-                            app.mention,
-                            f"https://t.me/{app.username}?start=sudolist",
-                            config.SUPPORT_CHAT,
-                        ),
-                        disable_web_page_preview=True,
-                    )
+
                     return await app.leave_chat(message.chat.id)
 
+                if message.chat.id in await blacklisted_chats():
+
+                    await message.reply_text(
+
+                        _["start_5"].format(
+
+                            app.mention,
+
+                            f"https://t.me/{app.username}?start=sudolist",
+
+                            config.SUPPORT_CHAT,
+
+                        ),
+
+                        disable_web_page_preview=True,
+
+                    )
+
+                    return await app.leave_chat(message.chat.id)
+
+
+
                 out = start_panel(_)
+
                 await message.reply_photo(
+
                     photo=config.START_IMG_URL,
+
                     caption=_["start_3"].format(
+
                         message.from_user.first_name,
+
                         app.mention,
+
                         message.chat.title,
+
                         app.mention,
+
                     ),
+
                     reply_markup=InlineKeyboardMarkup(out),
+
                 )
+
                 await add_served_chat(message.chat.id)
+
                 await message.stop_propagation()
+
         except Exception as ex:
+
             print(ex)
