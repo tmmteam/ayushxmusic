0d7334f65f10f3b20127e8430125d2b73f008578
tmmteam <teamtmm2@gmail.com>
2025-07-15 22:28:43 +0530

    Update mongo.py

--- AarohiX/core/mongo.py
+++ AarohiX/core/mongo.py
@@ -1,17 +1,35 @@
-import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
-import os
+from pyrogram import Client
 
-# Heroku config var se Mongo URI lena
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
+import config
+from ..logging import LOGGER
 
-if not MONGO_DB_URI:
-    raise Exception("❌ MONGO_DB_URI not found in environment variables!")
+TEMP_MONGODB = ""
 
-# Motor client for async access (for await calls)
-motor_client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
-mongodb = motor_client.get_default_database()
-
-# PyMongo client for normal access (non-await)
-pymongo_client = MongoClient(MONGO_DB_URI)
-pymongodb = pymongo_client.get_default_database()
+if not config.MONGO_DB_URI:
+    LOGGER(__name__).warning("No MONGO DB URL found. LOL")
+    
+    temp_client = Client(
+        "Anon",
+        bot_token=config.BOT_TOKEN,
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+    )
+    temp_client.start()
+    info = temp_client.get_me()
+    username = info.username
+    temp_client.stop()
+    
+    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+    _mongo_sync_ = MongoClient(TEMP_MONGODB)
+    
+    mongodb = _mongo_async_[username]
+    pymongodb = _mongo_sync_[username]
+else:
+    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+    
+    # 👇👇 Use the correct database name here instead of "Anon"
+    mongodb = _mongo_async_.get_default_database()
+    pymongodb = _mongo_sync_.get_default_database()
